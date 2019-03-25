ce74daf38553cf3859dc4fce526285f5425acee3
Erik Maximilian Forsman <Erik.Maximilian.Forsman@nav.no>
2019-03-25 15:21:55 +0100

    Exclude paths @ ensureSet

--- dusseldorf-ktor-core/src/main/kotlin/no/nav/helse/dusseldorf/ktor/core/CallIdExt.kt
+++ dusseldorf-ktor-core/src/main/kotlin/no/nav/helse/dusseldorf/ktor/core/CallIdExt.kt
@@ -2,14 +2,28 @@ package no.nav.helse.dusseldorf.ktor.core
 
 import io.ktor.features.CallId
 import io.ktor.http.HttpHeaders
+import io.ktor.request.header
+import io.ktor.request.path
 import java.util.*
 
 private const val NOT_SET = "NOT_SET"
+private const val EXCLUDED_PATH = "EXCLUDED_PATH"
 
 // Henter fra CorrelationID (backend tjenester)
-fun CallId.Configuration.fromXCorrelationIdHeader() {
-    retrieveFromHeader(HttpHeaders.XCorrelationId)
-    generate { NOT_SET }
+fun CallId.Configuration.fromXCorrelationIdHeader(
+        excludePaths : Set<String> = setOf("/metrics", "/isready", "/isalive", "/health")
+) {
+    retrieve { call ->
+        val headerValue = call.request.header(HttpHeaders.XCorrelationId)
+        when (headerValue) {
+            null -> {
+                if (excludePaths.contains(call.request.path())) EXCLUDED_PATH // En path som ikke må ha Correlation ID
+                else NOT_SET // Om headeren ikke er satt
+            }
+            EXCLUDED_PATH -> NOT_SET // Om noen har satt header til "EXCLUDE_PATH"
+            else -> headerValue // Verdien fra headeren
+        }
+    }
 }
 
 // Genererer CorrelationID (frontend tjeneste)
@@ -19,7 +33,7 @@ fun CallId.Configuration.generated() {
 
 fun CallId.Configuration.ensureSet() {
     verify { callId: String ->
-        if (callId.isEmpty() || callId == NOT_SET) {
+        if (callId == NOT_SET) {
             throw Throwblem(ValidationProblemDetails(
                     setOf(Violation(
                             parameterName = HttpHeaders.XCorrelationId,
