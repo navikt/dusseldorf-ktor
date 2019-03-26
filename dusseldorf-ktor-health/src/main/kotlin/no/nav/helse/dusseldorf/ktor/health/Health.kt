a7c6e80069e6a1948245778a3451c46aa0145c72
Erik Maximilian Forsman <Erik.Maximilian.Forsman@nav.no>
2019-03-26 12:43:47 +0100

    Helsesjekekr

---
+++ dusseldorf-ktor-health/src/main/kotlin/no/nav/helse/dusseldorf/ktor/health/Health.kt
@@ -0,0 +1,124 @@
+package no.nav.helse.dusseldorf.ktor.health
+
+import io.ktor.client.HttpClient
+import io.ktor.client.engine.apache.Apache
+import io.ktor.client.features.logging.Logging
+import io.ktor.client.request.HttpRequestBuilder
+import io.ktor.client.request.url
+import io.ktor.client.response.readText
+import io.ktor.http.HttpMethod
+import io.ktor.http.HttpStatusCode
+import kotlinx.coroutines.Deferred
+import kotlinx.coroutines.async
+import kotlinx.coroutines.awaitAll
+import kotlinx.coroutines.coroutineScope
+import no.nav.helse.dusseldorf.ktor.client.MonitoredHttpClient
+import no.nav.helse.dusseldorf.ktor.client.setProxyRoutePlanner
+import no.nav.helse.dusseldorf.ktor.client.sl4jLogger
+import org.slf4j.LoggerFactory
+import java.net.URL
+
+interface Result {
+    fun result() : Map<String, Any?>
+}
+
+class Healthy(private val result : Map<String, Any?> ) : Result {
+    constructor(message : String) : this(mapOf(Pair("message", message)))
+
+    override fun result(): Map<String, Any?> {
+        return result
+    }
+}
+
+class UnHealthy(private val result : Map<String, Any?> ) : Result {
+    constructor(message : String) : this(mapOf(Pair("message", message)))
+
+    override fun result(): Map<String, Any?> {
+        return result
+    }
+}
+
+interface HealthCheck {
+    suspend fun check() : Result
+}
+
+
+private val logger = LoggerFactory.getLogger("no.nav.helse.dusseldorf.ktor.health.HttpRequestHealthCheck")
+class HttpRequestHealthCheck(
+        private val app: String,
+        private val urlExpectedHttpStatusCodeMap : Map<URL, HttpStatusCode>
+) : HealthCheck {
+
+    private val monitoredHttpClient = MonitoredHttpClient(
+            source = app,
+            destination = "HttpRequestHealthCheck",
+            httpClient = HttpClient(Apache) {
+                engine {
+                    socketTimeout = 2_000  // Max time between TCP packets - default 10 seconds
+                    connectTimeout = 2_000 // Max time to establish an HTTP connection - default 10 seconds
+                    customizeClient { setProxyRoutePlanner() }
+                }
+                install (Logging) {
+                    sl4jLogger("HttpRequestHealthCheck")
+                }
+            }
+    )
+
+
+    override suspend fun check(): Result {
+        val responses = coroutineScope {
+            val futures = mutableListOf<Deferred<Response>>()
+            urlExpectedHttpStatusCodeMap.forEach { url, expectedHttpStatusCode ->
+                futures.add(async {
+                    val httpRequestBuilder = HttpRequestBuilder()
+                    httpRequestBuilder.method = HttpMethod.Get
+                    httpRequestBuilder.url(url)
+
+                    try {
+                        monitoredHttpClient.request(httpRequestBuilder).use {
+                            Response(
+                                    url = url,
+                                    expectedHttpStatusCode = expectedHttpStatusCode,
+                                    actualHttpStatusCode = it.status,
+                                    message = it.readText()
+                            )
+                        }
+                    } catch (cause: Throwable) {
+                        Response(
+                                url = url,
+                                expectedHttpStatusCode = expectedHttpStatusCode,
+                                actualHttpStatusCode = null,
+                                message = "Mottok ingen response. ${cause.message}"
+                        )
+                    }
+
+                })
+            }
+            futures.awaitAll()
+        }
+
+
+        val result = mutableMapOf<String, Any?>()
+        val containsUnhealthy = responses.none { it.isUnhealthy() }
+
+        responses.forEach { response ->
+            result[response.url.toString()] = mapOf(
+                    Pair("message", response.message),
+                    Pair("expected_http_status_code", response.expectedHttpStatusCode.value),
+                    Pair("actual_http_status_code", response.actualHttpStatusCode?.value)
+            )
+        }
+
+        return if (containsUnhealthy) UnHealthy(result) else Healthy(result)
+    }
+
+}
+
+private data class Response(
+        val url : URL,
+        val expectedHttpStatusCode: HttpStatusCode,
+        val actualHttpStatusCode: HttpStatusCode?,
+        val message: String? = null
+) {
+    internal fun isUnhealthy() : Boolean = expectedHttpStatusCode != actualHttpStatusCode
+}
---
+++ dusseldorf-ktor-health/src/main/kotlin/no/nav/helse/dusseldorf/ktor/health/HealthRoute.kt
@@ -0,0 +1,57 @@
+package no.nav.helse.dusseldorf.ktor.health
+
+import io.ktor.application.call
+import io.ktor.http.HttpStatusCode
+import io.ktor.response.respond
+import io.ktor.routing.Route
+import io.ktor.routing.get
+import kotlinx.coroutines.Deferred
+import kotlinx.coroutines.async
+import kotlinx.coroutines.awaitAll
+import kotlinx.coroutines.coroutineScope
+import org.slf4j.LoggerFactory
+
+private val logger = LoggerFactory.getLogger("no.nav.helse.dusseldorf.ktor.health.HealthRoute")
+
+fun Route.HealthRoute(
+        path: String = "/health",
+        healthChecks : Set<HealthCheck>
+) {
+
+    get(path) {
+        val results = coroutineScope {
+            val futures = mutableListOf<Deferred<Result>>()
+            healthChecks.forEach { healthCheck ->
+                futures.add(async {
+                    try {
+                        healthCheck.check()
+                    } catch (cause: Throwable) {
+                        logger.error("Feil ved eksekvering av helsesjekk.", cause)
+                        UnHealthy(message = cause.message ?: "Feil ved eksekvering av helsesjekk.")
+                    }
+                })
+
+            }
+            futures.awaitAll()
+        }
+
+        val healthy : MutableList<Map<String, Any?>> = mutableListOf()
+        val unhealthy : MutableList<Map<String, Any?>> = mutableListOf()
+
+        results.forEach { result ->
+            when (result) {
+                is Healthy -> healthy.add(result.result())
+                else -> unhealthy.add(result.result())
+            }
+        }
+
+
+        call.respond(
+                status = if (unhealthy.isEmpty()) HttpStatusCode.OK else HttpStatusCode.ServiceUnavailable,
+                message = mapOf(
+                    Pair("healthy", healthy),
+                    Pair("unhealthy", unhealthy)
+                )
+        )
+    }
+}
