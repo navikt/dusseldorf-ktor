66fc4715384c903378dfbfe178b99d92634fc5a4
Erik Maximilian Forsman <Erik.Maximilian.Forsman@nav.no>
2019-03-26 09:29:21 +0100

    Client logging

--- dusseldorf-ktor-client/src/main/kotlin/no/nav/helse/dusseldorf/ktor/client/HttpClientExt.kt
+++ dusseldorf-ktor-client/src/main/kotlin/no/nav/helse/dusseldorf/ktor/client/HttpClientExt.kt
@@ -1,9 +1,26 @@
 package no.nav.helse.dusseldorf.ktor.client
 
+import io.ktor.client.features.logging.Logger
+import io.ktor.client.features.logging.Logging
 import org.apache.http.impl.conn.SystemDefaultRoutePlanner
 import org.apache.http.impl.nio.client.HttpAsyncClientBuilder
+import org.slf4j.LoggerFactory
 import java.net.ProxySelector
 
 fun HttpAsyncClientBuilder.setProxyRoutePlanner() {
     setRoutePlanner(SystemDefaultRoutePlanner(ProxySelector.getDefault()))
+}
+fun Logging.Config.sl4jLogger(
+        name : String
+) {
+    logger = Sl4jLogger("no.nav.helse.dusseldorf.ktor.client.$name")
+}
+
+private class Sl4jLogger(
+        name : String
+) : Logger {
+    private val sl4j = LoggerFactory.getLogger(name)
+    override fun log(message: String) {
+        sl4j.trace(message)
+    }
 }
