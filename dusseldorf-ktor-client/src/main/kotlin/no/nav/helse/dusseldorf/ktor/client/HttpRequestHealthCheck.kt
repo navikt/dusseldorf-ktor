3aae577602efaaab5ce1e4008aa5057a4f062ab0
Erik Maximilian Forsman <Erik.Maximilian.Forsman@nav.no>
2019-06-02 22:51:43 +0200

    Helsesjekk m/ fuel

--- dusseldorf-ktor-client/src/main/kotlin/no/nav/helse/dusseldorf/ktor/client/HttpRequestHealthCheck.kt
+++ dusseldorf-ktor-client/src/main/kotlin/no/nav/helse/dusseldorf/ktor/client/HttpRequestHealthCheck.kt
@@ -1,12 +1,8 @@
 package no.nav.helse.dusseldorf.ktor.client
 
-import io.ktor.client.HttpClient
-import io.ktor.client.engine.apache.Apache
-import io.ktor.client.features.logging.Logging
-import io.ktor.client.request.HttpRequestBuilder
-import io.ktor.client.request.url
-import io.ktor.client.response.readText
-import io.ktor.http.HttpMethod
+import com.github.kittinunf.fuel.core.ResponseResultOf
+import com.github.kittinunf.fuel.coroutines.awaitStringResponseResult
+import com.github.kittinunf.fuel.httpGet
 import io.ktor.http.HttpStatusCode
 import kotlinx.coroutines.Deferred
 import kotlinx.coroutines.async
@@ -16,85 +12,80 @@ import no.nav.helse.dusseldorf.ktor.health.HealthCheck
 import no.nav.helse.dusseldorf.ktor.health.Healthy
 import no.nav.helse.dusseldorf.ktor.health.Result
 import no.nav.helse.dusseldorf.ktor.health.UnHealthy
-import org.slf4j.LoggerFactory
+import org.json.JSONArray
+import org.json.JSONException
+import org.json.JSONObject
 import java.net.URL
+import java.time.Duration
 
 class HttpRequestHealthCheck(
-        app: String,
         private val urlExpectedHttpStatusCodeMap : Map<URL, HttpStatusCode>
 ) : HealthCheck {
-    private val logger = LoggerFactory.getLogger("no.nav.helse.dusseldorf.ktor.client.HttpRequestHealthCheck")
 
-    private val monitoredHttpClient = MonitoredHttpClient(
-            source = app,
-            destination = "HttpRequestHealthCheck",
-            httpClient = HttpClient(Apache) {
-                engine {
-                    socketTimeout = 2_000  // Max time between TCP packets - default 10 seconds
-                    connectTimeout = 2_000 // Max time to establish an HTTP connection - default 10 seconds
-                    customizeClient { setProxyRoutePlanner() }
-                }
-                install (Logging) {
-                    sl4jLogger("HttpRequestHealthCheck")
-                }
-            }
-    )
-
+    private companion object {
+        private val timeout = Duration.ofSeconds(2).toMillisPart()
+    }
 
     override suspend fun check(): Result {
-        val responses = coroutineScope {
-            val futures = mutableListOf<Deferred<Response>>()
-            urlExpectedHttpStatusCodeMap.forEach { url, expectedHttpStatusCode ->
+        val triplets = coroutineScope {
+            val futures = mutableListOf<Deferred<ResponseResultOf<String>>>()
+            urlExpectedHttpStatusCodeMap.forEach { url, _ ->
                 futures.add(async {
-                    val httpRequestBuilder = HttpRequestBuilder()
-                    httpRequestBuilder.method = HttpMethod.Get
-                    httpRequestBuilder.url(url)
-
-                    try {
-                        monitoredHttpClient.request(httpRequestBuilder).use {
-                            Response(
-                                    url = url,
-                                    expectedHttpStatusCode = expectedHttpStatusCode,
-                                    actualHttpStatusCode = it.status,
-                                    message = it.readText()
-                            )
-                        }
-                    } catch (cause: Throwable) {
-                        logger.error("Ingen response mot $url", cause)
-                        Response(
-                                url = url,
-                                expectedHttpStatusCode = expectedHttpStatusCode,
-                                actualHttpStatusCode = null,
-                                message = "Mottok ingen response. ${cause.message}"
-                        )
-                    }
+                    url.toString()
+                            .httpGet()
+                            .timeout(timeout)
+                            .timeoutRead(timeout)
+                            .awaitStringResponseResult()
 
                 })
             }
             futures.awaitAll()
         }
 
+        val success = JSONObject()
+        val failure = JSONObject()
 
-        val result = mutableMapOf<String, Any?>()
-        val isHealthy = responses.none { it.isUnhealthy() }
+        triplets.forEach { (request,response,result) ->
+            val json = JSONObject()
+            val key = request.url.toString()
+            val expected = urlExpectedHttpStatusCodeMap.getValue(request.url).value
+            var actual : Any? = response.statusCode
 
-        responses.forEach { response ->
-            result[response.url.toString()] = mapOf(
-                    Pair("message", response.message),
-                    Pair("expected_http_status_code", response.expectedHttpStatusCode.value),
-                    Pair("actual_http_status_code", response.actualHttpStatusCode?.value)
+            val message = result.fold(
+                    { success ->
+                        success.jsonOrString()
+                        actual = response.statusCode
+                    },
+                    { error ->
+                        if (error.cause != null) {
+                            actual = null // TODO: remove?
+                            error.toString()
+                        }
+                        else { (String(error.response.data).jsonOrString()) }
+                    }
             )
+
+            json.put("message", message)
+            json.put("expected_http_status_code", expected)
+            json.put("actual_http_status_code", actual)
+
+            if (expected == actual) success.put(key, json) else failure.put(key, json)
         }
 
-        return if (isHealthy) Healthy(name = "HttpRequestHealthCheck", result = result) else UnHealthy(name = "HttpRequestHealthCheck", result = result)
+        val json = JSONObject()
+        json.put("success", success)
+        json.put("failure", failure)
+
+        return if (failure.isEmpty) Healthy(name = "HttpRequestHealthCheck", result = json)
+        else UnHealthy(name = "HttpRequestHealthCheck", result = json)
     }
 }
 
-private data class Response(
-        val url : URL,
-        val expectedHttpStatusCode: HttpStatusCode,
-        val actualHttpStatusCode: HttpStatusCode?,
-        val message: String? = null
-) {
-    internal fun isUnhealthy() : Boolean = expectedHttpStatusCode != actualHttpStatusCode
+
+private fun String.jsonOrString() : Any {
+    return try { JSONObject(this) }
+    catch (cause: JSONException) {
+        try { JSONArray(this)
+        } catch (cause : JSONException) { this }
+    }
 }
