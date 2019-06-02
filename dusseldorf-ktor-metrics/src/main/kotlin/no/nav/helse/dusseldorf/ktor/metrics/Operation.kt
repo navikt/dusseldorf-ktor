d2d3bad0729025fe0b17998697b1b18b1a6103ee
Erik Maximilian Forsman <Erik.Maximilian.Forsman@nav.no>
2019-06-02 23:37:58 +0200

    operation

--- dusseldorf-ktor-metrics/src/main/kotlin/no/nav/helse/dusseldorf/ktor/metrics/Operation.kt
+++ dusseldorf-ktor-metrics/src/main/kotlin/no/nav/helse/dusseldorf/ktor/metrics/Operation.kt
@@ -8,28 +8,29 @@ class Operation {
         private val histogram = Histogram
                 .build("monitored_operation_histogram",
                         "Histogram som måler operasjoner.")
-                .labelNames("operation")
+                .labelNames("app", "operation")
                 .register()
 
         private val counter = Counter
                 .build(
                         "monitored_operation_counter",
                         "Teller for alle målte operasjoenr.")
-                .labelNames("operation", "result")
+                .labelNames("app", "operation", "result")
                 .register()
 
         suspend fun <T> monitored(
-                name: String,
+                app: String,
+                operation: String,
                 resultResolver: (T) -> Boolean = { true },
                 block: suspend () -> T
         ) : T {
-            val timer = histogram.labels(name).startTimer()
+            val timer = histogram.labels(app, operation).startTimer()
             return try {
                 val result = block()
-                counter.labels(name, resultResolver(result).toResult())
+                counter.labels(app, operation, resultResolver(result).toResult())
                 result
             } catch (cause: Throwable) {
-                counter.labels(name, false.toResult()).inc()
+                counter.labels(app, operation, false.toResult()).inc()
                 throw cause
             } finally {
                 timer.observeDuration()
