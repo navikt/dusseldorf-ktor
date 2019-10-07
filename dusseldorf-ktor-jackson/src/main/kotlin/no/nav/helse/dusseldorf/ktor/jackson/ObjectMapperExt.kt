4fa349a057239bd186a85500e389c6717dd88bff
Erik Maximilian Forsman <Erik.Maximilian.Forsman@nav.no>
2019-10-07 13:19:19 +0200

    Jackson 2.10.* serialiserer duration som timestamps by default.

--- dusseldorf-ktor-jackson/src/main/kotlin/no/nav/helse/dusseldorf/ktor/jackson/ObjectMapperExt.kt
+++ dusseldorf-ktor-jackson/src/main/kotlin/no/nav/helse/dusseldorf/ktor/jackson/ObjectMapperExt.kt
@@ -9,6 +9,7 @@ import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule
 fun ObjectMapper.dusseldorfConfigured() : ObjectMapper {
     configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, false)
     configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false)
+    configure(SerializationFeature.WRITE_DURATIONS_AS_TIMESTAMPS, false)
     propertyNamingStrategy = PropertyNamingStrategy.SNAKE_CASE
     registerModule(JavaTimeModule())
     return this
