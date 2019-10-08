13df376d64493eb845615ff4e4802110a2f1760c
Erik Maximilian Forsman <Erik.Maximilian.Forsman@nav.no>
2019-10-08 21:45:57 +0200

    Forsøker deploy-file

--- scripts/get-version.sh
+++ scripts/get-version.sh
@@ -4,4 +4,4 @@ ktorVersionLine=$(grep '<ktor\.version>' pom.xml)
 ktorVersion=$(echo $ktorVersionLine | sed 's/<ktor.version>\(.*\)<\/ktor.version>/\1/g')
 gitShortHash=$(git rev-parse --short HEAD)
 newProjectVersion="${ktorVersion}.${gitShortHash}"
-echo "${gitShortHash}"
+echo "${newProjectVersion}"
---
+++ scripts/maven-deploy.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+repositoryId=github
+url=https://maven.pkg.github.com/navikt/dusseldorf-ktor
+groupId=no.nav.helse
+version=$(./scripts/get-version.sh)
+
+deploy() {
+    if [ "$1" == "pom" ]; then
+        file=pom.xml
+    else
+        file=$2/target/$2-$version.$1
+    fi
+
+    echo ./mvnw --settings .github/settings.xml deploy:deploy-file \
+      -DgroupId=$groupId \
+      -DartifactId=$2 \
+      -Dversion=$version \
+      -Dpackaging=$1 \
+      -Dfile=$file \
+      -DrepositoryId=$repositoryId \
+      -Durl=$url \
+      -DgeneratePom=false
+}
+
+deploy pom dusseldorf-ktor
+deploy jar dusseldorf-ktor-core
+deploy jar dusseldorf-ktor-metrics
+deploy jar dusseldorf-ktor-client
+deploy jar dusseldorf-ktor-jackson
+deploy jar dusseldorf-ktor-health
+deploy jar dusseldorf-oauth2-client
+deploy jar dusseldorf-ktor-auth
+deploy jar dusseldorf-ktor-test-support
