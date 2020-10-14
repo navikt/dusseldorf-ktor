b2a1bbcf5e658a941b5962a38ebbe7eb2eab4e59
Erik Maximilian Forsman <Erik.Maximilian.Forsman@nav.no>
2020-10-14 10:48:48 +0200

    Funksjon for å merge flere Health Results.

--- dusseldorf-ktor-health/src/main/kotlin/no/nav/helse/dusseldorf/ktor/health/Health.kt
+++ dusseldorf-ktor-health/src/main/kotlin/no/nav/helse/dusseldorf/ktor/health/Health.kt
@@ -2,6 +2,28 @@ package no.nav.helse.dusseldorf.ktor.health
 
 interface Result {
     fun result() : Map<String, Any?>
+    companion object {
+        fun merge(name: String, vararg results: Result) : Result {
+            val healthy = mutableListOf<Map<String, Any?>>()
+            val unHealthy = mutableListOf<Map<String, Any?>>()
+
+            results.forEach { result -> when (result) {
+                is Healthy -> healthy.add(result.result())
+                else -> unHealthy.add(result.result())
+            }}
+
+            val merged = mapOf(
+                "name" to name,
+                "healthy" to healthy,
+                "unhealthy" to unHealthy
+            )
+
+            return when (unHealthy.isEmpty()) {
+                true -> Healthy(merged)
+                false -> UnHealthy(merged)
+            }
+        }
+    }
 }
 
 class Healthy(private val result : Map<String, Any?> ) : Result {
