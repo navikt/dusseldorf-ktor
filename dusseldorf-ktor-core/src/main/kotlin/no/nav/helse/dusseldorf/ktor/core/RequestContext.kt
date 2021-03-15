c97d683f2711cbcb3de4b4f6ccc6dee9fc3a0708
Erik Maximilian Forsman <Erik.Maximilian.Forsman@nav.no>
2021-03-15 09:58:50 +0100

    Lagt til RequestContext

---
+++ dusseldorf-ktor-core/src/main/kotlin/no/nav/helse/dusseldorf/ktor/core/RequestContext.kt
@@ -0,0 +1,52 @@
+package no.nav.helse.dusseldorf.ktor.core
+
+import io.ktor.application.*
+import io.ktor.features.*
+import io.ktor.http.*
+import io.ktor.request.*
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.withContext
+import kotlin.coroutines.AbstractCoroutineContextElement
+import kotlin.coroutines.CoroutineContext
+import kotlin.coroutines.coroutineContext
+
+object RequestContext {
+
+    data class Context(private val context: Map<String, Any>) {
+        fun getRequired(key: String) = context.getValue(key)
+        fun getOptional(key: String) = context[key]
+        fun correlationId() = getRequired(HttpHeaders.XCorrelationId)
+        fun authorizationHeader() = getRequired(HttpHeaders.Authorization)
+    }
+
+    private fun Map<String, Any?>.filterNotNullValues() = filterValues { it != null }.mapValues { it.value!! }
+
+    fun CoroutineContext.requestContext() =
+        get(CoroutineRequestContext.Key)?.context ?: throw IllegalStateException("Request Context ikke satt.")
+
+    suspend fun <T>withRequestContext(
+        call: ApplicationCall,
+        block: suspend CoroutineScope.() -> T) = withContext(
+        context = coroutineContext + CoroutineRequestContext(Context(call.initializeRequestContext().filterNotNullValues())),
+        block = block
+    )
+
+    fun ApplicationCall.initializeRequestContext() = mapOf(
+        HttpHeaders.XCorrelationId to (callId ?: request.header(HttpHeaders.XCorrelationId)),
+        HttpHeaders.Authorization to (request.authorization() ?: request.header(HttpHeaders.Authorization))
+    )
+
+    suspend fun <T>withRequestContext(
+        context: Map<String, Any?>,
+        block: suspend CoroutineScope.() -> T) = withContext(
+        context = coroutineContext + CoroutineRequestContext(Context(context.filterNotNullValues())),
+        block = block
+    )
+
+    private class CoroutineRequestContext(
+        val context: Context
+    ) : AbstractCoroutineContextElement(Key) {
+        companion object Key : CoroutineContext.Key<CoroutineRequestContext>
+    }
+}
+
