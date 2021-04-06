4dda69a3ded0760a4824dc42529f4b059c9166d0
Erik Maximilian Forsman <Erik.Maximilian.Forsman@nav.no>
2021-04-06 09:49:28 +0200

    Dependency updates. Closes #232, Closes #237, Closes #238

--- gradle/dusseldorf-ktor.gradle.kts
+++ gradle/dusseldorf-ktor.gradle.kts
@@ -1,5 +1,5 @@
-val ktorVersion by extra("1.5.2")
-val kotlinVersion by extra("1.4.31")
+val ktorVersion by extra("1.5.3")
+val kotlinVersion by extra("1.4.32")
 val logbackVersion by extra("1.2.3")
 val logstashLogbackVersion by extra("6.6")
 val prometheusVersion by extra("0.10.0")
