43eea28de3cb6a4cf4acbe4a9b7860a438434058
Ramin Esfandiari <ramin_esfandiari_93@hotmail.com>
2021-10-08 12:29:27 +0200

    Legger på `iat` på signedJwt.

--- dusseldorf-oauth2-client/src/main/kotlin/no/nav/helse/dusseldorf/oauth2/client/SignedJwtAccessTokenClient.kt
+++ dusseldorf-oauth2-client/src/main/kotlin/no/nav/helse/dusseldorf/oauth2/client/SignedJwtAccessTokenClient.kt
@@ -18,15 +18,15 @@ import java.security.MessageDigest
 import java.security.interfaces.RSAPrivateKey
 
 class SignedJwtAccessTokenClient(
-        private val clientId: String,
-        privateKeyProvider: PrivateKeyProvider,
-        keyIdProvider: KeyIdProvider,
-        private val tokenEndpoint: URI,
-        private val onBehalfOfGrantType: GrantType = tokenEndpoint.grantType()
+    private val clientId: String,
+    privateKeyProvider: PrivateKeyProvider,
+    keyIdProvider: KeyIdProvider,
+    private val tokenEndpoint: URI,
+    private val onBehalfOfGrantType: GrantType = tokenEndpoint.grantType()
 ) : AccessTokenClient, NimbusAccessTokenClient() {
     private val jwsSigner: JWSSigner
-    private val algorithm : JWSAlgorithm = JWSAlgorithm.RS256
-    private val jwsHeader : JWSHeader
+    private val algorithm: JWSAlgorithm = JWSAlgorithm.RS256
+    private val jwsHeader: JWSHeader
 
     private companion object {
         private val logger = LoggerFactory.getLogger(SignedJwtAccessTokenClient::class.java)
@@ -35,18 +35,18 @@ class SignedJwtAccessTokenClient(
     init {
         jwsSigner = RSASSASigner(privateKeyProvider.getPrivateKey())
         jwsHeader = JWSHeader.Builder(algorithm)
-                .keyID(keyIdProvider.getKeyId())
-                .type(JOSEObjectType.JWT)
-                .build()
+            .keyID(keyIdProvider.getKeyId())
+            .type(JOSEObjectType.JWT)
+            .build()
         logger.info("OnBehalfOfGrantType=$onBehalfOfGrantType")
     }
 
 
-    override fun getAccessToken(scopes: Set<String>) : AccessTokenResponse {
+    override fun getAccessToken(scopes: Set<String>): AccessTokenResponse {
         return getAccessToken(getClientCredentialsTokenRequest(scopes))
     }
 
-    override fun getAccessToken(scopes: Set<String>, onBehalfOf: String) : AccessTokenResponse {
+    override fun getAccessToken(scopes: Set<String>, onBehalfOf: String): AccessTokenResponse {
         return when (onBehalfOfGrantType) {
             GrantType.JwtBearer -> getAccessToken(getOnBehalfOfJwtBearerTokenRequest(onBehalfOf, scopes))
             GrantType.TokenExchange -> getAccessToken(getOnBehalfOfTokenExchangeTokenRequest(onBehalfOf, scopes))
@@ -55,12 +55,12 @@ class SignedJwtAccessTokenClient(
 
     // https://docs.microsoft.com/en-us/azure/active-directory/develop/v2-oauth2-client-creds-grant-flow#second-case-access-token-request-with-a-certificate
     private fun getClientCredentialsTokenRequest(
-            scopes: Set<String>
-    ) : TokenRequest = TokenRequest(
-            tokenEndpoint,
-            PrivateKeyJWT(getSignedJwt()),
-            ClientCredentialsGrant(),
-            getScope(scopes)
+        scopes: Set<String>
+    ): TokenRequest = TokenRequest(
+        tokenEndpoint,
+        PrivateKeyJWT(getSignedJwt()),
+        ClientCredentialsGrant(),
+        getScope(scopes)
     )
 
     // https://docs.microsoft.com/en-us/azure/active-directory/develop/v2-oauth2-on-behalf-of-flow#second-case-access-token-request-with-a-certificate
@@ -79,47 +79,56 @@ class SignedJwtAccessTokenClient(
     // https://doc.nais.io/security/auth/tokenx/#exchanging-a-token
     private fun getOnBehalfOfTokenExchangeTokenRequest(
         onBehalfOf: String,
-        scopes: Set<String>) = TokenExchange(
-            tokenEndpoint = tokenEndpoint,
-            privateKeyJWT = PrivateKeyJWT(getSignedJwt()),
-            scope = checkNotNull(getScope(scopes)) { "Token Exchange kan ikke gjøres uten scope" },
-            onBehalfOf = SignedJWT.parse(onBehalfOf)
-        ).toHTTPRequest()
+        scopes: Set<String>
+    ) = TokenExchange(
+        tokenEndpoint = tokenEndpoint,
+        privateKeyJWT = PrivateKeyJWT(getSignedJwt()),
+        scope = checkNotNull(getScope(scopes)) { "Token Exchange kan ikke gjøres uten scope" },
+        onBehalfOf = SignedJWT.parse(onBehalfOf)
+    ).toHTTPRequest()
 
-    private fun getSignedJwt() : SignedJWT {
+    private fun getSignedJwt(): SignedJWT {
         val jwtClaimSet = JWTClaimsSet.Builder()
-                .audience(tokenEndpoint.toString())
-                .subject(clientId)
-                .issuer(clientId)
-                .jwtID(UUID.randomUUID().toString())
-                .notBeforeTime(getNotBeforeTime())
-                .expirationTime(getExpirationTime())
-                .build()
+            .audience(tokenEndpoint.toString())
+            .subject(clientId)
+            .issuer(clientId)
+            .jwtID(UUID.randomUUID().toString())
+            .notBeforeTime(getNotBeforeTime())
+            .expirationTime(getExpirationTime())
+            .issueTime(getIssuedAtTime())
+            .build()
 
         val signedJwt = SignedJWT(jwsHeader, jwtClaimSet)
         signedJwt.sign(jwsSigner)
         return signedJwt
     }
 
-    private fun getNotBeforeTime() : Date {
+    private fun getNotBeforeTime(): Date {
         val now = LocalDateTime.now(Clock.systemUTC())
         return Date.from(now.toInstant(ZoneOffset.UTC))
     }
 
-    private fun getExpirationTime() : Date {
+    private fun getIssuedAtTime(): Date {
+        val now: LocalDateTime = LocalDateTime.now(Clock.systemUTC())
+        return Date.from(now.toInstant(ZoneOffset.UTC))
+    }
+
+    private fun getExpirationTime(): Date {
         val exp = LocalDateTime.now(Clock.systemUTC()).plusSeconds(10)
         return Date.from(exp.toInstant(ZoneOffset.UTC))
     }
 }
 
-interface KeyIdProvider{
-    fun getKeyId() : String
+interface KeyIdProvider {
+    fun getKeyId(): String
 }
+
 class DirectKeyId(private val keyId: String) : KeyIdProvider {
     override fun getKeyId(): String = keyId
 }
+
 class FromCertificateHexThumbprint(private val hexThumbprint: String) : KeyIdProvider {
-    override fun getKeyId(): String  = Base64.getUrlEncoder().encodeToString(hexStringToByteArray(hexThumbprint))
+    override fun getKeyId(): String = Base64.getUrlEncoder().encodeToString(hexStringToByteArray(hexThumbprint))
     private fun hexStringToByteArray(s: String): ByteArray {
         val len = s.length
         val data = ByteArray(len / 2)
@@ -131,6 +140,7 @@ class FromCertificateHexThumbprint(private val hexThumbprint: String) : KeyIdPro
         return data
     }
 }
+
 class FromCertificatePem(private val pem: String) : KeyIdProvider {
     override fun getKeyId(): String {
         val certificateFactory = CertificateFactory.getInstance("X.509")
@@ -144,16 +154,19 @@ class FromCertificatePem(private val pem: String) : KeyIdProvider {
 }
 
 interface PrivateKeyProvider {
-    fun getPrivateKey() : RSAPrivateKey
+    fun getPrivateKey(): RSAPrivateKey
 }
+
 class DirectPrivateKey(private val privateKey: RSAPrivateKey) : PrivateKeyProvider {
     override fun getPrivateKey(): RSAPrivateKey = privateKey
 }
+
 class FromJwk(private val jwk: String) : PrivateKeyProvider {
     override fun getPrivateKey(): RSAPrivateKey {
         return RSAKey.parse(jwk).toRSAPrivateKey()
     }
 }
+
 class FromPrivateKeyPem(private val pem: String) : PrivateKeyProvider {
     override fun getPrivateKey(): RSAPrivateKey {
         val jwk = JWK.parseFromPEMEncodedObjects(pem)
