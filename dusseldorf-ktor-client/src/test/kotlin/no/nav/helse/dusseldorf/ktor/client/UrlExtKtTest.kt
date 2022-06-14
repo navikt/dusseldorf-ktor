a615e0cd38696e407239902c9017a5a73a011c34
Ramin Esfandiari <ramin_esfandiari_93@hotmail.com>
2022-06-14 12:05:10 +0200

    Fikser buildUrl. (#449)
    * Fikser buildUrl.
    
    Tester for ønsket oppførsel.
    
    * Fjerner println

---
+++ dusseldorf-ktor-client/src/test/kotlin/no/nav/helse/dusseldorf/ktor/client/UrlExtKtTest.kt
@@ -0,0 +1,24 @@
+package no.nav.helse.dusseldorf.ktor.client
+
+import io.ktor.http.Url
+import org.junit.Assert.assertEquals
+import org.junit.Test
+import java.net.URI
+
+class UrlExtKtTest {
+
+    @Test
+    fun `gitt url med path forvent riktig url når det legges til en ny path`() {
+        val completeUrl = Url.buildURL(
+            baseUrl = URI("http://localhost:8080"),
+            pathParts = listOf("v1", "dokument")
+        )
+
+        val urlMedDokumentId = Url.buildURL(
+            baseUrl = completeUrl,
+            pathParts = listOf("123")
+        ).toString()
+
+        assertEquals("http://localhost:8080/v1/dokument/123", urlMedDokumentId)
+    }
+}
