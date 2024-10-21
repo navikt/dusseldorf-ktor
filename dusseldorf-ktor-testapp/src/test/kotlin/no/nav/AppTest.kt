58162150df3a7afcc2e27d7de54b6b7b7d3540f6
Ramin Esfandiari <ramin_esfandiari_93@hotmail.com>
2024-10-21 11:02:43 +0200

    Løser kompilaskjonsfeil

--- dusseldorf-ktor-testapp/src/test/kotlin/no/nav/AppTest.kt
+++ dusseldorf-ktor-testapp/src/test/kotlin/no/nav/AppTest.kt
@@ -1,13 +1,20 @@
 package no.nav
 
-import io.ktor.client.plugins.*
-import io.ktor.http.*
-import io.ktor.server.engine.*
-import io.ktor.server.netty.*
-import kotlinx.coroutines.*
+import io.ktor.client.plugins.timeout
+import io.ktor.http.HttpStatusCode
+import io.ktor.server.engine.applicationEnvironment
+import io.ktor.server.engine.connector
+import io.ktor.server.engine.embeddedServer
+import io.ktor.server.netty.Netty
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.cancelAndJoin
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
 import no.nav.helse.dusseldorf.ktor.client.SimpleHttpClient.httpGet
 import no.nav.helse.dusseldorf.ktor.client.SimpleHttpClient.readTextOrThrow
-import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
 
 internal class AppTest {
@@ -16,9 +23,13 @@ internal class AppTest {
     fun `hente metrics & pre-stop`() {
         withNettyEngine(appPort = 1337) {
             "http://localhost:1337/".let {
-                doNTimes(n=2) { GlobalScope.launch(Dispatchers.IO) { "${it}treg-request".httpGet{ builder ->
-                    builder.timeout { requestTimeoutMillis = 10000 }
-                }.second.getOrThrow() } }
+                doNTimes(n = 2) {
+                    GlobalScope.launch(Dispatchers.IO) {
+                        "${it}treg-request".httpGet { builder ->
+                            builder.timeout { requestTimeoutMillis = 10000 }
+                        }.second.getOrThrow()
+                    }
+                }
                 doNTimes {
                     assertEquals(HttpStatusCode.OK, "${it}metrics".httpGet().readTextOrThrow().first)
                     assertEquals(HttpStatusCode.OK, "${it}proxied-metrics".httpGet().readTextOrThrow().first)
@@ -30,15 +41,22 @@ internal class AppTest {
         }
     }
 
-    private suspend fun doNTimes(n: Int = 20,block: suspend () -> Any) {
-        for (i in 1..n) { block() }
+    private suspend fun doNTimes(n: Int = 20, block: suspend () -> Any) {
+        for (i in 1..n) {
+            block()
+        }
     }
 
     private fun withNettyEngine(appPort: Int, block: suspend () -> Unit) {
-        val server = embeddedServer(Netty, applicationEngineEnvironment {
-            module { app() }
-            connector { port = appPort }
-        })
+        val server = embeddedServer(
+            factory = Netty,
+            environment = applicationEnvironment {},
+            module = { app() },
+            configure = {
+                connector { port = appPort }
+            }
+        )
+
         val job = GlobalScope.launch {
             server.start(wait = true)
         }
@@ -55,7 +73,7 @@ internal class AppTest {
         try {
             runBlocking { block() }
         } finally {
-            server.stop(1000,1000)
+            server.stop(1000, 1000)
             runBlocking { job.cancelAndJoin() }
         }
     }
