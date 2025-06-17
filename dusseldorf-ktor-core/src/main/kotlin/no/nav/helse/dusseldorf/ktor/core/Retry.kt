6f9960e291897e01954cdb80a99658282345b838
Tore Langedal Endestad <tore.langedal.endestad@nav.no>
2025-06-17 13:08:09 +0200

    Endrer exception-håndtering i Retry. Kast videre CancellationException. Ikke fang Error (#906)

--- dusseldorf-ktor-core/src/main/kotlin/no/nav/helse/dusseldorf/ktor/core/Retry.kt
+++ dusseldorf-ktor-core/src/main/kotlin/no/nav/helse/dusseldorf/ktor/core/Retry.kt
@@ -1,9 +1,11 @@
 package no.nav.helse.dusseldorf.ktor.core
 
 import kotlinx.coroutines.delay
+import kotlinx.coroutines.ensureActive
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 import java.time.Duration
+import kotlin.coroutines.coroutineContext
 
 class Retry {
     companion object {
@@ -14,7 +16,7 @@ class Retry {
                 maxDelay: Duration = Duration.ofSeconds(1),
                 factor: Double = 2.0,
                 logger: Logger = LoggerFactory.getLogger("no.nav.helse.dusseldorf.ktor.core.Retry"),
-                exceptionHandler: (Throwable) -> Unit = {},
+                exceptionHandler: (Exception) -> Unit = {},
                 block: suspend () -> T): T
         {
             var currentDelayInMillis = initialDelay.toMillis()
@@ -25,7 +27,10 @@ class Retry {
                     val result = block()
                     logger.gikkOk(operation = operation, currentTry = currentTry, tries = tries)
                     return result
-                } catch (cause: Throwable) {
+                } catch (cause: Exception) {
+                    //https://medium.com/better-programming/the-silent-killer-thats-crashing-your-coroutines-9171d1e8f79b
+                    coroutineContext.ensureActive() //kaster CancellationException videre dersom kansellert ordentlig
+
                     logger.warn("Feil ved '$operation'. ${cause.javaClass.simpleName} med melding '${cause.message}'")
                     exceptionHandler(cause)
                 }
